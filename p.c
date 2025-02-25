5bbd7e823880d7d12fafe0e2ad6d85c252c0f0e9
22Pedro22 <pedroaaavvv@gmail.com>
2025-02-25 16:50:57 -0300

    versão1.0

--- p.c
+++ p.c
@@ -37,4 +37,27 @@ void email(){
         printf("> ");
         scanf("%hu" , &choice);
         if(choice == 1){
+		                srand(time(NULL));
+                unsigned int num = rand() % 1001;
+                char utilizers[][10] = {"Network" , "User_Name" , "Anonymous"};
+                unsigned int num1 = rand() % 1001;
+                char subdomains[][20] = {"@gmail.com" , "@outlook.com" , "@hotmail.com" , "@yahoo.com"};
+                unsigned int num_utilizers = rand() % 3;
+                unsigned int num_subdomains = rand() % 4;
+                printf("Email gerado: %u%s%u%s\n" , num , utilizers[num_utilizers] , num1 , subdomains[num_subdomains]);
+
+        }
+
+}
+
+void passwd(){
+        unsigned short int choice;
+        printf("--------------\n");
+        printf("[1] 8 Caracteres\n");
+        printf("[2] 16 Caracteres\n");
+        printf("[3] 32 Caracteres\n");
+        printf("> ");
+        scanf("%hu" , &choice);
+
+}
    
